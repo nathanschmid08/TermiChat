799b4ec1ff2f2271fa02c81c4b99ee9babc4689b
nathanschmid08 <nathandion.schmid@gmail.com>
2025-05-14 15:07:15 +0200

    changed the window size to original terminal

--- CHAT/main.js
+++ CHAT/main.js
@@ -6,8 +6,8 @@ let currentDir = process.env.HOME;
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 600,
+        height: 384,
         webPreferences: {
             contextIsolation: false,
             nodeIntegration: true
